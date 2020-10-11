555b50a195c91ce95dbd7982d42e51c219e1ee33
Kraktu <57345348+Kraktu@users.noreply.github.com>
2020-10-11 10:10:51 +0200

    MainMenuJumpRepaired

---
+++ Assets/Scripts/MenuJumpScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuJumpScript : MonoBehaviour
+{
+	public bool isJumping;
+		private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.layer == 8)
+		{
+			isJumping = false;
+		}
+	}
+	private void OnCollisionExit(Collision collision)
+	{
+		if (collision.gameObject.layer == 8)
+		{
+			isJumping = true;
+		}
+	}
+}
