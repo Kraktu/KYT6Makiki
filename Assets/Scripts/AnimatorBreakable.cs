fbadff2060b9c0073113e07a29f57507bae092b4
rascar <to.constant@gmail.com>
2020-10-21 14:41:41 +0200

    Fix start balloon animation

--- Assets/Scripts/AnimatorBreakable.cs
+++ Assets/Scripts/AnimatorBreakable.cs
@@ -6,11 +6,15 @@ using RasPacJam.Audio;
 
 public class AnimatorBreakable : MonoBehaviour
 {
+    [SerializeField] private bool isStartAnimation = false;
+    [SerializeField] private Vector3 addedHeight = new Vector3(0f, 100f, 0f);
+	[SerializeField] private float fallTime = 1f;
     [SerializeField] private float rotatingSpeed = 0.05f;
     [SerializeField] private Vector3 rotation = new Vector3(0f, 0f, 2f);
     [SerializeField] private float explosionDelay = 0.3f;
     [SerializeField] private ParticleSystem explosionParticlesPrefab = null;
     private Tween rotationTween;
+	private Coroutine falling;
 
 
 
@@ -37,10 +41,44 @@ public class AnimatorBreakable : MonoBehaviour
     {
         Instantiate(explosionParticlesPrefab,
                 GetComponent<Renderer>().bounds.center, Quaternion.identity);
-        gameObject.SetActive(false);
+        if(isStartAnimation)
+        {
+            Despawn();
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 
+	public void Despawn()
+	{
+		if (falling!=null)
+		{
+			StopCoroutine(falling);
+		}
 
+		transform.position = transform.position + addedHeight;
+		falling = StartCoroutine(Fall());
+	}
+
+
+
+	private IEnumerator Fall()
+	{
+		float time = 0;
+		Vector3 startingPos = transform.position;
+		Vector3 endingPos = transform.position - addedHeight;
+		float tRatio;
+		while (time<fallTime)
+		{
+			tRatio = time / fallTime;
+			transform.position = Vector3.Lerp(startingPos, endingPos, tRatio);
+			time += Time.deltaTime;
+			yield return null;
+		}
+		transform.position = endingPos;
+	}
 
     private void Start()
     {
