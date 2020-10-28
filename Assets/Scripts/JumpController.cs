9108a8f4bc8a309c7fe8895099b3422ef301c9df
rascar <to.constant@gmail.com>
2020-10-28 14:28:42 +0100

    Add hold jump

--- Assets/Scripts/JumpController.cs
+++ Assets/Scripts/JumpController.cs
@@ -6,10 +6,15 @@ using RasPacJam.Audio;
 public class JumpController : MonoBehaviour
 {
     public bool CanJump { get => canJump; set => canJump = value; }
-    [SerializeField] private float jumpHeight = 10f;
     [SerializeField] private GroundChecker groundChecker = null;
+    [SerializeField] private float maxJumpHeight = 10f;
+    [SerializeField] private float maxJumpTime = 2f;
+    [SerializeField] private bool hasMomentum = false;
     private Rigidbody rb;
+    private float jumpTimeCounter;
+    private float jumpIntensity;
     private bool canJump;
+    private bool isJumping;
 
 
 
@@ -24,15 +29,48 @@ public class JumpController : MonoBehaviour
             return;
         }
 
-        rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        isJumping = true;
         AudioManager.Instance.Play("jump");
     }
 
+    public void Land()
+    {
+        if(!isJumping)
+        {
+            return;
+        }
+
+        isJumping = false;
+        if(!hasMomentum)
+        {
+            rb.velocity = Vector3.zero;
+        }
+        jumpTimeCounter = maxJumpTime;
+    }
 
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        jumpTimeCounter = maxJumpTime;
+        jumpIntensity = maxJumpHeight / maxJumpTime;
         canJump = true;
+        isJumping = false;
+    }
+
+    private void Update()
+    {
+        if(!isJumping)
+        {
+            return;
+        }
+
+        rb.velocity = Vector3.up * jumpIntensity;
+
+        jumpTimeCounter -= Time.deltaTime;
+        if(jumpTimeCounter <= 0)
+        {
+            Land();
+        }
     }
 }
