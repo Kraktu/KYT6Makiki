a6a27fa7957a5b502903941d646c1d51c7c03618
rascar <to.constant@gmail.com>
2020-11-01 01:19:21 +0100

    [WIP] add brightness selection

---
+++ Assets/Scripts/BrightnessSelector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
+
+public class BrightnessSelector : MonoBehaviour
+{
+    [SerializeField] private PostProcessVolume postProcessVolume = null;
+    [SerializeField] private Slider brightnessSlider = null;
+    private ColorGrading colorGrading;
+
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        postProcessVolume.profile.TryGetSettings<ColorGrading>(out colorGrading);
+    }
+
+    private void Update()
+    {
+        if(colorGrading)
+        {
+            colorGrading.brightness.value = brightnessSlider.value * 200 - 100;
+        }
+    }
+}
